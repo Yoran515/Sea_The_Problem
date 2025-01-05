04f8785e06d83b3c32cf0839b82ce75ccdbcd8a0
turtleluc <turtleluczuidema@gmail.com>
2025-01-05 21:25:11 +0100

    TaskTest

--- Assets/TaskHandler.cs
+++ Assets/TaskHandler.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class TaskHandler : MonoBehaviour
 {
 
-    [SerializeField] private List<GameObject> tasks = new List<GameObject>();
+    [SerializeField] private List<Task> tasks = new List<Task>();
 
+    [SerializeField] private List<List<Task>> waves = new List<List<Task>>();
+
+    private int currentWave = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,5 +21,40 @@ public class TaskHandler : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        WaveHandler();
+    }
+
+    void WaveHandler()
+    {
+        if (currentWave < waves.Count)
+        {
+            bool allTasksCompleted = true;
+            foreach (Task task in waves[currentWave])
+            {
+                if (!task.IsCompleted) 
+                {
+                    allTasksCompleted = false;
+                    break;
+                }
+            }
+
+            if (allTasksCompleted)
+            {
+                currentWave++;
+                Debug.Log("Wave " + (currentWave) + " completed!");
+            }
+        }
+        else
+        {
+            Debug.Log("All waves completed!");
+        }
+    }
+    void CheckTaskStatus()
+    {
+
     }
 }
